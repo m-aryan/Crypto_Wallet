0faab7c0b4caf20178dc58c6df30fbaaba2fd4c3
Aryan M. <aryanmanek4@gmail.com>
2025-03-06 18:36:54 +0530

    first commit

---
+++ src/test/java/com/ary/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ary.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
